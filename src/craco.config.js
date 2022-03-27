6d8019887311c7d06131251d594991ebc53f7e64
nithinjose-tech <ninjos8@gmail.com>
2022-03-27 19:56:55 +0530

    npm modules2

--- src/craco.config.js
+++ src/craco.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     babel: {
       loaderOptions: {
-        ignore: ["./node_modules"],
+        ignore: ["./node_modules/react-map-gl/dist/es6/mapbox/mapbox.js"],
       },
     },
   };
